13a3a86024f25ea153ed7db81a855bea1b3fbef1
vivivi <v.castro05@ufromail.cl>
2025-05-24 23:21:35 -0400

    fix controllers files names

--- src/routes/alertas.routes.js
+++ src/routes/alertas.routes.js
@@ -1,16 +1,2 @@
 const {Alerta}=require('../../models/PuntoMapa')
 
-const postAlerta = async (req, res) =>{
-};
-
-const getAlertas = async (req, res) =>{
-};
-
-const getAlertasActivas = async (req, res) =>{
-};
-
-const getAlerta = async (req, res) =>{
-};
-
-const patchAlerta = async (req, res) =>{
-};
