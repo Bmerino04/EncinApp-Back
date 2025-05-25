04a30602c439ee82ed3fc7a0a28858e6dfdd2801
vivivi <v.castro05@ufromail.cl>
2025-05-25 15:51:58 -0400

    add router index

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import usuarioRouter from './usuario.routes.js';
+import alertaRouter from './alertas.routes.js';
+import anuncioRouter from './anuncios.routes.js';
+import comentarioAlertaRouter from './comentarioAlerta.routes.js';
+import puntosInteresRouter from './puntosInteres.routes.js';
+
+const router = express.Router();
+
+router.use('/usuarios', usuarioRouter);
+router.use('/alertas', alertaRouter);
+router.use('/anuncios', anuncioRouter);
+router.use('/comentarios-alerta', comentarioAlertaRouter);
+router.use('/puntos-interes', puntosInteresRouter);
+
+export default router; 
