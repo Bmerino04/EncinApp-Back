1f0261e819bf91875d5b987bc7da4da2574bdabe
vivivi <v.castro05@ufromail.cl>
2025-05-31 02:06:38 -0400

    build: add waitForDb class
    waitForDb class tries to connect to the container every few seconds

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import db from './models/index.js';
 import rutas from './routes/index.js';
+import waitForDb from './utils/waitForDb.js';
 import dotenv from 'dotenv';
-dotenv.config();
+const { config } = dotenv;
 
 const app = express();
 
@@ -10,13 +11,16 @@ app.use(express.json());
 
 app.use('/api', rutas);
 
-db.sequelize.sync({ alter: true })
-  .then(() => {
-    console.log('Base de datos sincronizada');
-    app.listen(3000, () => {
-      console.log('Servidor escuchando en puerto 3000');
-    });
-  })
-  .catch(err => {
-    console.error('Error al conectar con la base de datos:', err);
-  });
+(async () => {
+    try {
+        await waitForDb(db.sequelize);
+        await db.sequelize.sync({ alter: true });
+        console.log('Base de datos sincronizada');
+        app.listen(3000, () => {
+            console.log('Servidor escuchando en puerto 3000');
+        });
+    } catch (err) {
+        console.error('Error al conectar con la base de datos:', err);
+        process.exit(1);
+    }
+})();
