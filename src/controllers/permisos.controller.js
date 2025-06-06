ac32d95907650364fd15927948a6a25880d05d33
Bmerino04 <bmerinomunoz0404@gmail.com>
2025-06-06 03:17:24 -0400

    feat: update actualizarPermisosUsuarios to support empty lists

--- src/controllers/permisos.controller.js
+++ src/controllers/permisos.controller.js
@@ -1,67 +1,85 @@
 import db from "../models/index.js";
 const { usuario, permiso } = db;
 
-
 async function obtenerPermisosUsuario(request, response) {
-    try {
-        const usuarioId = request.params.id;
+  try {
+    const usuarioId = request.params.id;
 
-        const usuarioEncontrado = await usuario.findByPk(usuarioId, {
-            include: {
-                model: permiso,
-                as: 'permisos',
-                attributes: ['id_permiso', 'nombre'],
-                through: {
-                    attributes: [] // Excluye los atributos de la tabla intermedia
-                }
-            }
-        });
+    const usuarioEncontrado = await usuario.findByPk(usuarioId, {
+      include: {
+        model: permiso,
+        as: "permisos",
+        attributes: ["id_permiso", "nombre"],
+        through: {
+          attributes: [],
+        },
+      },
+    });
 
-        if (!usuarioEncontrado) {
-            return response.status(404).json({ message: 'Usuario no encontrado' });
-        }
-
-        if (usuarioEncontrado.permisos.length === 0) {
-            return response.status(404).json({ message: 'El usuario no tiene permisos asignados' });
-        }
-
-        return response.status(200).json({ permisos: usuarioEncontrado.permisos });
-    } catch (error) {
-        return response.status(500).json({ error: 'Error al obtener los permisos del usuario', detalle: error.message });
+    if (!usuarioEncontrado) {
+      return response.status(404).json({ message: "Usuario no encontrado" });
     }
-}  
+
+    if (usuarioEncontrado.permisos.length === 0) {
+      return response
+        .status(404)
+        .json({ message: "El usuario no tiene permisos asignados" });
+    }
+
+    return response.status(200).json({ permisos: usuarioEncontrado.permisos });
+  } catch (error) {
+    return response.status(500).json({
+      error: "Error al obtener los permisos del usuario",
+      detalle: error.message,
+    });
+  }
+}
 
 async function actualizarPermisosUsuario(request, response) {
-    try {
-        const usuarioId = request.params.id;
-        const  nombrePermisos = request.body;
+  try {
+    const usuarioId = request.params.id;
+    const nombrePermisos = request.body;
 
-        if (!Array.isArray(nombrePermisos) || nombrePermisos.length === 0) {
-            return response.status(400).json({ message: 'Debe proporcionar una lista de permisos válida' });
-        }
-
-        const usuarioExistente = await usuario.findByPk(usuarioId);
-        if (!usuarioExistente) {
-            return response.status(404).json({ message: 'Usuario no encontrado' });
-        }
-
-        const permisos = await permiso.findAll({
-            where: {
-                nombre: nombrePermisos
-            }
-        });
-
-        if (permisos.length !== nombrePermisos.length) {
-            return response.status(400).json({ message: 'Algunos permisos no existen' });
-        }
-
-        // Actualiza los permisos del usuario
-        await usuarioExistente.setPermisos(permisos);
-
-        return response.status(200).json({ message: 'Permisos actualizados correctamente' });
-    } catch (error) {
-        return response.status(500).json({ error: 'Error al actualizar los permisos del usuario' });
+    if (!Array.isArray(nombrePermisos)) {
+      return response
+        .status(400)
+        .json({ message: "Debe proporcionar una lista de permisos válida" });
     }
+
+    const usuarioExistente = await usuario.findByPk(usuarioId);
+    if (!usuarioExistente) {
+      return response.status(404).json({ message: "Usuario no encontrado" });
+    }
+
+    if (nombrePermisos.length === 0) {
+      await usuarioExistente.setPermisos([]);
+      return response
+        .status(200)
+        .json({ message: "Permisos removidos correctamente" });
+    }
+
+    const permisos = await permiso.findAll({
+      where: {
+        nombre: nombrePermisos,
+      },
+    });
+
+    if (permisos.length !== nombrePermisos.length) {
+      return response
+        .status(400)
+        .json({ message: "Algunos permisos no existen" });
+    }
+
+    await usuarioExistente.setPermisos(permisos);
+
+    return response
+      .status(200)
+      .json({ message: "Permisos actualizados correctamente" });
+  } catch (error) {
+    return response
+      .status(500)
+      .json({ error: "Error al actualizar los permisos del usuario" });
+  }
 }
 
 export { obtenerPermisosUsuario, actualizarPermisosUsuario };
