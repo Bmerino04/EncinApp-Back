18be5bf4ac6fd4273cc00c3919a806481d6f1b26
vivivi <v.castro05@ufromail.cl>
2025-06-12 03:45:20 -0400

    feat: add presidencia middleware to verify the presidence atribute before manage user permissions

---
+++ src/middleware/presidencia.middleware.js
@@ -0,0 +1,26 @@
+import db from '../models/index.js';
+const { usuario } = db;
+
+export default function verificarPresidencia() {
+    return async (request, response, next) => {
+        try{
+            const usuarioId = request.user.id;
+
+            const usuarioEncontrado =  await  usuario.findByPk(usuarioId)
+
+            if (!usuarioEncontrado) {
+                return response.status(404).json({ message: 'Usuario no encontrado' });
+            }
+
+            if (usuarioEncontrado.es_presidente) {
+                next();
+            } else {
+                return response.status(403).json({ message: 'No tienes permisos de presidencia' });
+            }
+            
+
+        } catch (error) {
+            return response.status(500).json({ error: 'Error al verificar la presidencia', detalle: error.message });
+        }   
+    }
+}
