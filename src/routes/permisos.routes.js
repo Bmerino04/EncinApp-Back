5d1e1146bc8c50dcd9269c55b33463d8cbab60ec
vivivi <v.castro05@ufromail.cl>
2025-06-12 03:43:40 -0400

    feat: add transferirPresidencia route to transfer the presidence permissions

--- src/routes/permisos.routes.js
+++ src/routes/permisos.routes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import {  obtenerPermisosUsuario, actualizarPermisosUsuario } from "../controllers/permisos.controller.js";
+import {  obtenerPermisosUsuario, actualizarPermisosUsuario, transferirPresidencia } from "../controllers/permisos.controller.js";
 import verificarToken from "../middleware/auth.middleware.js";
+import verificarPresidencia from "../middleware/presidencia.middleware.js";
 
 const permisosRouter = express.Router();
 
-permisosRouter.use(verificarToken);
+
+permisosRouter.use(verificarToken, verificarPresidencia);
+
+permisosRouter.patch("/:id", transferirPresidencia);
 
 permisosRouter.get("/:id", obtenerPermisosUsuario);
 
