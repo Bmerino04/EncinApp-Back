77af9d460b497ad61ef6a163b629fb7f369d6ba9
vivivi <v.castro05@ufromail.cl>
2025-06-18 00:28:49 -0400

    feat: asign gestionar_puntos permission to puntoInteres routes

--- src/routes/puntosInteres.routes.js
+++ src/routes/puntosInteres.routes.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import { crearPuntoInteres, obtenerPuntoInteres, obtenerPuntosInteres, eliminarPuntoInteres} from '../controllers/puntoInteres.controller.js';
 import verificarToken from '../middleware/auth.middleware.js';
+import verificarPermiso from '../middleware/permisos.middleware.js';
 
 const puntoInteresRouter = express.Router();
 puntoInteresRouter.use(verificarToken);
 
-puntoInteresRouter.post('/', crearPuntoInteres);
+puntoInteresRouter.post('/', verificarPermiso('gestionar_puntos'), crearPuntoInteres);
 
 puntoInteresRouter.get('/:id', obtenerPuntoInteres);
 
 puntoInteresRouter.get('/', obtenerPuntosInteres);
 
-puntoInteresRouter.delete('/:id', eliminarPuntoInteres);
+puntoInteresRouter.delete('/:id', verificarPermiso('gestionar_puntos'),eliminarPuntoInteres);
 
 export default puntoInteresRouter;
