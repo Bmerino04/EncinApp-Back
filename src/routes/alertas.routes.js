7bee325e8845f59943a752e8bbdbb87aa3e6842b
vivivi <v.castro05@ufromail.cl>
2025-06-18 00:28:10 -0400

    feat: add recomendarPuntoInteres route

--- src/routes/alertas.routes.js
+++ src/routes/alertas.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { crearAlerta, obtenerAlerta, obtenerAlertas, desactivarAlerta } from '../controllers/alerta.controller.js';
 import { crearComentarioAlerta, obtenerComentariosAlerta, eliminarComentarioAlerta} from '../controllers/comentarioAlerta.controller.js';
-
+import { recomendarPuntoInteres } from '../controllers/puntoInteres.controller.js';
 import verificarToken from '../middleware/auth.middleware.js';
 import verificarPermiso from '../middleware/permisos.middleware.js';
 
@@ -21,6 +21,8 @@ alertaRouter.post('/:id/comentarios', crearComentarioAlerta);
 alertaRouter.get('/:id/comentarios', obtenerComentariosAlerta);
 
 alertaRouter.delete('/:idAlerta/comentarios/:idComentario', verificarPermiso('gestionar_alertas'), eliminarComentarioAlerta);
+
+alertaRouter.get('/:id/recomendaciones', recomendarPuntoInteres);
 
 export default alertaRouter;
 
